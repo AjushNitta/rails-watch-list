be71863829ff52c944478139331a02c782b7df1d
ajushnittarajiv <xajushnitta.411@gmail.com>
2022-11-20 23:00:02 +0100

    remove destroy db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-puts 'cleaning database'
-Movie.destroy_all
+#puts 'cleaning database'
+#Movie.destroy_all
 
 puts 'creating movies'
 
